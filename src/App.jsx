0181fc09613bcb18b6880eadaac5cfc0b734c89a
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-02-17 16:34:50 -0300

    Adding x on cell click

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import { useState } from 'react';
 import { Box, Typography,Container, } from '@mui/material'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(""))
+  const [board, setBoard] = useState(Array(9).fill(0))
+
+  const handleCellClick = (cellIndex) => {
+    setBoard(
+      board.map((item, index) => {
+        if (index === cellIndex){
+          return 1
+        }
+        return item
+      })
+    )
+  }
   return (
     <Container 
       sx={
@@ -22,21 +33,37 @@ function App() {
           gap: "20px",
         }}
       >
-        {board.map((index) => (
-          <Box
-            key={index}
-            sx={{
-              width: 240,
-              height: 240,
-              backgroundColor: 'primary.light',
-              '&:hover': {
-                backgroundColor: 'primary.dark',
-                opacity: [0.9, 0.8, 0.7],       
-              },
-            }}
-          >
-        </Box>
-        ))}
+        {board.map((item, index) => {
+          let emoji;
+          console.log(index)
+          if (item === 1){
+            emoji = "✖️"
+          }
+          else if (item === 2){
+            emoji = "⭕"
+          }
+          else {
+            emoji = ""
+          }
+          return(
+            <Box
+              key={index}
+              onClick={() => handleCellClick(index)}
+              sx={{
+                width: 240,
+                height: 240,
+                backgroundColor: 'primary.light',
+                fontSize: "6rem",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                '&:hover': {
+                  backgroundColor: 'primary.dark',
+                  opacity: [0.9, 0.8, 0.7],       
+                },
+              }}
+            >{emoji}</Box>
+        )})}
       </Box>
 
     </Container>
