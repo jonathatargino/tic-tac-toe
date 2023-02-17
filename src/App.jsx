c717818fe26f98829af35615598a923b853f5430
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-02-17 19:38:01 -0300

    Win Dialog, Reseting page when closes dialog

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,64 @@
 import './App.css'
 import { useEffect, useState } from 'react';
-import { Box, Typography,Container, } from '@mui/material'
+import { Container, Typography, Box, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button } from '@mui/material'
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(0))
   const [player, setPlayer] = useState(1);
+  const [winner, setWinner] = useState(0);
+  const [openDialog, setOpenDialog] = useState(false);
 
   useEffect(() => {
-    handleWin();
+    checkWin();
   }, [board])
+
+  useEffect(() => {
+    if (winner !== 0){
+      handleOpenDialog();
+    }
+  }, [winner])
 
 
   const handleCellClick = (cellIndex) => {
-    setBoard(
-      board.map((item, index) => {
-        // Se o index da célula iterada for igual ao index da célula clicada, o array receberá o valor do player atual no lugar do 0. Ou seja, se o player 1 clicar numa célula, o array irá receber o valor 1 no index correspondente à célula.
-        if (index === cellIndex & item === 0){
-          return player
-        }
-        return item
-      })
-    )
-    handlePlayerChange();
+    if (winner === 0){
+      setBoard(
+        board.map((item, index) => {
+          // Se o index da célula iterada for igual ao index da célula clicada, o array receberá o valor do player atual no lugar do 0. Ou seja, se o player 1 clicar numa célula, o array irá receber o valor 1 no index correspondente à célula.
+          if (index === cellIndex & item === 0){
+            return player
+          }
+          return item
+        })
+      )
+      handlePlayerChange();
+    }
   }
 
   const handlePlayerChange = () => {
     setPlayer(player === 1 ? 2 : 1);
   }
 
-const handleWin = () => {
-  winningWays.forEach((way) => {
-    if(way.every(cell => board[cell] === 1)){
-      console.log('Player 1 venceu.')
-    }
-    else if(way.every(cell => board[cell] === 2)){
-      console.log('Player 2 venceu.')
-    }
-  })
-}
+  const checkWin = () => {
+    winningWays.forEach((way) => {
+      if(way.every(cell => board[cell] === 1)){
+        setWinner(1)
+        return true;
+      }
+      else if(way.every(cell => board[cell] === 2)){
+        setWinner(2)
+      }
+    })
+  }
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true)
+  }
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false)
+    location.reload();
+  }
+
 
   const winningWays = [
     [0,1,2], [3,4,5], [6,7,8], // Horizontal
@@ -85,15 +106,26 @@ const handleWin = () => {
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
+                cursor: winner === 0 ? 'pointer' : 'not-allowed',
                 '&:hover': {
-                  backgroundColor: 'primary.dark',
-                  opacity: [0.9, 0.8, 0.7],       
+                  backgroundColor: winner === 0 ? 'primary.dark' : 'primary.ligth',
+                  opacity: winner === 0 ? [0.9, 0.8, 0.7] : 1,       
                 },
               }}
             >{emoji}</Box>
         )})}
       </Box>
-
+      <Dialog
+        open={openDialog}
+        onClose={handleCloseDialog}
+      >
+        <DialogContent>
+          <DialogTitle >{`Jogador ${winner} venceu o jogo!`}</DialogTitle>  
+          <DialogActions sx={{display: "flex", justifyContent: "center"}}>
+            <Button onClick={handleCloseDialog}>Jogar Novamente</Button>
+          </DialogActions> 
+        </DialogContent>    
+      </Dialog>
     </Container>
   )
 }
