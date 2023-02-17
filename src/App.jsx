49e1f6945abe7fd3a8160323b7ca9a7a06f5598b
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-02-17 18:53:15 -0300

    ways to win, console log when one player wins

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Box, Typography,Container, } from '@mui/material'
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(0))
   const [player, setPlayer] = useState(1);
+
+  useEffect(() => {
+    handleWin();
+  }, [board])
+
 
   const handleCellClick = (cellIndex) => {
     setBoard(
@@ -22,10 +27,27 @@ function App() {
   const handlePlayerChange = () => {
     setPlayer(player === 1 ? 2 : 1);
   }
+
+const handleWin = () => {
+  winningWays.forEach((way) => {
+    if(way.every(cell => board[cell] === 1)){
+      console.log('Player 1 venceu.')
+    }
+    else if(way.every(cell => board[cell] === 2)){
+      console.log('Player 2 venceu.')
+    }
+  })
+}
+
+  const winningWays = [
+    [0,1,2], [3,4,5], [6,7,8], // Horizontal
+    [0,3,6], [1,4,7], [2,5,8], // Vertical
+    [0,4,8], [2,4,6] // Diagonal
+  ]
   return (
     <Container 
       sx={
-        {display: "flex", flexDirection: "column", gap: "20px", alignItems: "center"}
+        {display: "flex", flexDirection: "column", gap: "10px", alignItems: "center"}
       }>
 
       <Typography variant='h1' component='h1'>
@@ -42,7 +64,6 @@ function App() {
       >
         {board.map((item, index) => {
           let emoji;
-          console.log(index)
           if (item === 1){
             emoji = "✖️"
           }
