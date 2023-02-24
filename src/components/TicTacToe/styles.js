31b7bbc3445ca6a1e189a8d4816022d7915ccc2a
Luiz <luizferreirafariasn@gmail.com>
2023-02-24 14:47:26 -0300

    Responsive Container and GridBox (board)

--- src/components/TicTacToe/styles.js
+++ src/components/TicTacToe/styles.js
@@ -5,15 +5,28 @@ const ContainerProps = {
   flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
-  gap: "20px"
+  gap: "20px",
 }
 
 
 const GridBoxProps = {
   display: "grid",
-  gridTemplateColumns: "repeat(3, 260px)",
-  gridTemplateRows: "repeat(3, 260px)",
-  gap: "20px",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridTemplateRows: "repeat(3, 1fr)",
+  gap: {
+    xs: "10px",
+    sm: "20px",
+  },
+  width: {
+    xs: "60vw",
+    sm: "40vw",
+    xl: "30vw",
+  },
+  height: {
+    xs: "60vw",
+    sm: "40vw",
+    xl: "30vw",
+  }
 }
 
 
